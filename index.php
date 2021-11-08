821f334da10093cb4948bc5c40ee5b50176e6faa
Tuong2001 <tuongdvgcc19257@fpt.edu.vn>
2021-11-08 13:47:16 +0700

    t

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ include_once("connection.php");
                     </ul>
                 </li>
                 
-            
+                <li><a href="?page=contact">Contact</a></li>
             </ul>
         </nav>
         <div id="mobile-menu-wrap"></div>
@@ -256,12 +256,18 @@ include_once("connection.php");
                              <nav class="header__menu">
                     
                              <ul>
-                                 <li class="active"><a href="?page=content">Home</a></li>
-                                 <li><a href="?page=shop-grid">Shop</a></li>
-                                 
-                                
-                                 <li><a href="?page=contact">Contact</a></li>
-                                 <li><a href="?page=shopping-cart">Shopping Cart</a></li>
+                             <li class="active"><a href="?page=content">Home</a></li>
+                <li><a href="?page=shop-grid">Shop</a></li>
+                <li><a href="?page=content">Pages</a>
+                    <ul class="header__menu__dropdown">
+                        
+                        <li><a href="?page=shopping-cart">Shopping Cart</a></li>
+                        <li><a href="?page=checkout">Check Out</a></li>
+                        
+                    </ul>
+                </li>
+                
+                <li><a href="?page=contact">Contact</a></li>
                                  
                              </ul> 
                              
