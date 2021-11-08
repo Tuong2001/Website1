f4444e06fca6f99aa407d59ea49f06cd4b72fbc8
Tuong2001 <tuongdvgcc19257@fpt.edu.vn>
2021-11-08 07:03:07 +0700

    t

--- Add_Category.php
+++ Add_Category.php
@@ -6,10 +6,8 @@
                         <div class="hero__categories__all">
                             <i class="fa fa-bars"></i>
                             <span>All toy brands</span>
-                            
-                            <ul>
-                        <?php Department($conn); ?>
-                            
+                        <ul>
+                            <?php Department($conn); ?>   
                         </ul>
                         </div>
                         
